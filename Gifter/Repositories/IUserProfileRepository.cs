130f41be2eaf57648b0a9dce8e2739be12dc9d4b
John Price <johndouglasprice9@gmail.com>
2021-05-28 11:33:36 -0400

    Finished ch 3

---
+++ Gifter/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gifter.Models;
+using System.Collections.Generic;
+
+namespace Gifter.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile profile);
+        void Delete(int id);
+        List<UserProfile> GetAll();
+        UserProfile GetById(int id);
+        UserProfile GetByIdWithPosts(int id);
+        void Update(UserProfile profile);
+    }
+}
